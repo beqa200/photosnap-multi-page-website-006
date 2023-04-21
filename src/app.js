87c58a61bf98bd9a153edd16b2fded5db7fdec51
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-04-21 19:19:07 +0200

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('.burger-menu')
+const nav = document.querySelector('nav')
 const menuIcon = document.querySelector('.burger-menu').children
 menuBtn.addEventListener('click', () => {
     menuIcon[0].classList.toggle('rotateX')
     menuIcon[1].classList.toggle('rotateY')
+    nav.classList.toggle('is-active')
 })
