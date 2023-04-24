e951f7b18d69331c4021ed8fdf962ca01b181e69
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-04-24 23:22:38 +0200

    adgdggdagd

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector('.burger-menu')
+const menuBtn = document.querySelector('.burger-menu-icon')
 const nav = document.querySelector('nav')
-const menuIcon = document.querySelector('.burger-menu').children
+const menuIcon = document.querySelector('.burger-menu-icon').children
 menuBtn.addEventListener('click', () => {
     menuIcon[0].classList.toggle('rotateX')
     menuIcon[1].classList.toggle('rotateY')
